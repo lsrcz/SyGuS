ab8fe2397d43c2f8b12fe367950a63270bdc060f
Sirui Lu <lsrcz@pku.edu.cn>
2018-12-29 16:32:01 +0800

    quality control
    Signed-off-by: Sirui Lu <lsrcz@pku.edu.cn>

--- programs/decisiontree/main.py
+++ programs/decisiontree/main.py
@@ -3,14 +3,15 @@ import sys
 from possibleterm import findPossibleValue
 from semantics import exprFromList
 from task import SynthTask
-from treelearning import TreeLearner
+from treelearning import TreeLearner, genmoreconstraint
 
 
 def main():
     task = SynthTask(sys.argv[1])
-    possiblevalue = list(map(lambda l: exprFromList(l, task.functionProto),
+    possiblevalue = list(map(lambda l: exprFromList(l, [task.functionProto]),
                              findPossibleValue(task.ins.bmExpr)))
-    treelearner = TreeLearner(task.ins.semchecker, task.ins.z3checker, possiblevalue)
+    moreconstraint = genmoreconstraint(task.productions, task.ins.inputlist, task.ins.inputtylist)
+    treelearner = TreeLearner(task.ins.semchecker, task.ins.z3checker, possiblevalue, moreconstraint)
     print(treelearner.mainalgo())
     #expr = Expr('ite', Expr('>=', Expr('x'), Expr('y')), Expr('x'), Expr('y'))
     #print(sem.check(expr, {'x': 1, 'y': 2}))
